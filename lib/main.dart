8d3f8feeca3cb934edf77970164a964a4a1cacdd
Wilder Trujillo <nadie437@gmail.com>
2021-02-28 15:52:35 -0500

    instalando provider como dependencia y configurandolo en nuestro home

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:qr_reader/src/pages/home_page.dart';
 import 'package:qr_reader/src/pages/mapa_page.dart';
+import 'package:qr_reader/src/providers/ui_provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Material App',
-      initialRoute: 'home',
-      routes: {
-        'home': (BuildContext context) => HomePage(),
-        'mapa': (BuildContext context) => MapaPage(),
-      },
-      theme: ThemeData(
-        primaryColor: Colors.deepPurple,
-        floatingActionButtonTheme: FloatingActionButtonThemeData(
-          backgroundColor: Colors.deepPurple,
-          //fuentes iconos se pueden cambiar
+    return MultiProvider(
+      providers: [
+        //el create es una funcion que se va a ejecutar cuando no hay ninguna instancia de providers creado
+        ChangeNotifierProvider(create: (_) => new UiProvider()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Material App',
+        initialRoute: 'home',
+        routes: {
+          'home': (BuildContext context) => HomePage(),
+          'mapa': (BuildContext context) => MapaPage(),
+        },
+        theme: ThemeData(
+          primaryColor: Colors.deepPurple,
+          floatingActionButtonTheme: FloatingActionButtonThemeData(
+            backgroundColor: Colors.deepPurple,
+            //fuentes iconos se pueden cambiar
+          ),
         ),
       ),
     );
--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:qr_reader/src/pages/direcciones_page.dart';
 import 'package:qr_reader/src/pages/mapas_pages.dart';
+import 'package:qr_reader/src/providers/ui_provider.dart';
 import 'package:qr_reader/src/widgets/custom_navigatorbar.dart';
 import 'package:qr_reader/src/widgets/scan_button.dart';
 
@@ -28,7 +30,10 @@ class HomePage extends StatelessWidget {
 class _HomePageBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final currentIndex = 0;
+    final uiProvider = Provider.of<UiProvider>(context);
+
+    //cambiar para mostrar la página respectuva
+    final currentIndex = uiProvider.selectedMenuOpt;
 
     switch (currentIndex) {
       case 0:
