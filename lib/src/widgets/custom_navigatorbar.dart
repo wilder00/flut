03087b7a9db1d070e7461418dde524a7b61b8202
Wilder Trujillo <nadie437@gmail.com>
2021-02-28 16:01:22 -0500

    cambiando la vista con el provider de mapas a direcciones, termiando

--- lib/src/widgets/custom_navigatorbar.dart
+++ lib/src/widgets/custom_navigatorbar.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:qr_reader/src/providers/ui_provider.dart';
 
 class CustomNavigationBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final currentIndex = 0;
+    final uiProvider = Provider.of<UiProvider>(context);
+    final currentIndex = uiProvider.selectedMenuOpt;
 
     return BottomNavigationBar(
+      // hacemos uiProvider.selectedMenuOpt = i, porque selectedMenuOpt es un setter
+      onTap: (int i) => uiProvider.selectedMenuOpt = i,
       //nos dirá cuál es elemento seleccionado
       currentIndex: currentIndex,
       elevation: 1,
