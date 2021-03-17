6fc1738ca32632da95d94c9292c880058cfd09af
Wilder Trujillo <nadie437@gmail.com>
2021-03-17 17:13:07 -0500

    optimizando widget con scan_tiles.dart

--- lib/src/pages/mapas_pages.dart
+++ lib/src/pages/mapas_pages.dart
@@ -1,39 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:qr_reader/src/providers/scan_list_provider.dart';
+import 'package:qr_reader/src/widgets/scan_tiles.dart';
 
 class MapasPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    //aqui si se necesita escuchar los cambios para que se redibuje
-    final scanListProvider =
-        Provider.of<ScanListProvider>(context, listen: true);
-
-    final scans = scanListProvider.scans;
-
-    return ListView.builder(
-      itemCount: scans.length,
-      itemBuilder: (_, i) => Dismissible(
-        key: UniqueKey(),
-        background: Container(color: Colors.red),
-        onDismissed: (DismissDirection direction) {
-          Provider.of<ScanListProvider>(context, listen: false)
-              .borrarScanPorId(scans[i].id);
-        },
-        child: ListTile(
-          leading: Icon(
-            Icons.map,
-            color: Theme.of(context).primaryColor,
-          ),
-          title: Text(scans[i].valor),
-          subtitle: Text(scans[i].id.toString()),
-          trailing: Icon(
-            Icons.keyboard_arrow_right,
-            color: Colors.grey,
-          ),
-          onTap: () => print("hacer algo"),
-        ),
-      ),
-    );
+    return ScanTiles(tipo: "geo");
   }
 }
---
+++ lib/src/widgets/scan_tiles.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:qr_reader/src/providers/scan_list_provider.dart';
+
+class ScanTiles extends StatelessWidget {
+  final String tipo;
+
+  const ScanTiles({Key key, @required this.tipo}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    //aqui si se necesita escuchar los cambios para que se redibuje
+    final scanListProvider =
+        Provider.of<ScanListProvider>(context, listen: true);
+
+    final scans = scanListProvider.scans;
+
+    return ListView.builder(
+      itemCount: scans.length,
+      itemBuilder: (_, i) => Dismissible(
+        key: UniqueKey(),
+        background: Container(color: Colors.red),
+        onDismissed: (DismissDirection direction) {
+          Provider.of<ScanListProvider>(context, listen: false)
+              .borrarScanPorId(scans[i].id);
+        },
+        child: ListTile(
+          leading: Icon(
+            this.tipo == "http" ? Icons.home_outlined : Icons.map,
+            color: Theme.of(context).primaryColor,
+          ),
+          title: Text(scans[i].valor),
+          subtitle: Text(scans[i].id.toString()),
+          trailing: Icon(
+            Icons.keyboard_arrow_right,
+            color: Colors.grey,
+          ),
+          onTap: () => print("hacer algo"),
+        ),
+      ),
+    );
+  }
+}
